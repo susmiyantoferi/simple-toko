e7b712d1912f08a06f4cfa35742e2ea81044ba10
Feri Susmiyanto <irefmcr@gmail.com>
2025-08-20 19:34:09 +0700

    deploy

---
+++ Dockerfile
@@ -0,0 +1,16 @@
+FROM golang:1.24.6-bookworm AS builder
+WORKDIR /app
+
+COPY go.mod go.sum ./
+RUN go mod download
+
+COPY . .
+
+RUN CGO_ENABLED=0 go build -trimpath -ldflags="-w" -o /goapp .
+
+FROM debian:bookworm-slim
+WORKDIR /app
+
+COPY --from=builder /app/goapp .
+
+CMD ["./goapp" ]
--- repository/payment_repository.go
+++ repository/payment_repository.go
@@ -12,5 +12,6 @@ type PaymentRepository interface {
 	FindByOrderId(ctx context.Context, orderId uint) (*entity.Payment, error)
 	FindAll(ctx context.Context) ([]*entity.Payment, error)
 	Delete(ctx context.Context, id uint) error
+	//Confirm(ctx context.Context, orderId uint, pym *entity.Payment) (*entity.Payment, error)
 	//UpdatePayment(ctx context.Context, pym *entity.Payment) (*entity.Payment, error)
 }
